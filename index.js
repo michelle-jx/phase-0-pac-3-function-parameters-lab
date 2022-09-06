d8dbeab556354f2f8473dbfcd6a620346083bb03
Michelle Too <mjxtcodes@gmail.com>
2022-09-05 17:31:45 -0700

    commit tree c67a28f1c194c17ec813f142b6b7ce95f6cecb54

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name) {
+    return(`Hi, my name is ${name}.`);
+}
+
+
+function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+function introductionWithLanguageOptional(name, language) {
+    let name === `Gracie`
+    if (language === `Python`){
+        console.log(name, language)
+    } else if (langugage === `Javascript`) {
+        console.log(name, language)
+    } 
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`) 
+}
